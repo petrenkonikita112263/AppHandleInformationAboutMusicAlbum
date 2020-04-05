5bd06b562065cc6929aeeefef0fd55d6e89faa81
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-04-05 16:52:05 +0300

    Optimized imports and deleted unused code (comments)

--- src/main/java/com/spring/api/music_project/model/service/ConverterService.java
+++ src/main/java/com/spring/api/music_project/model/service/ConverterService.java
@@ -1,20 +1,13 @@
 package com.spring.api.music_project.model.service;
 
-import com.spring.api.music_project.model.AlbumSummary;
 import com.spring.api.music_project.model.convert.ContentConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.List;
-
 @Service
 public class ConverterService {
-
 
     @Bean
     public ConversionService conversionService() {
@@ -22,22 +15,4 @@ public class ConverterService {
         service.addConverter(new ContentConverter());
         return service;
     }
-
-
-//    public void doConvert() {
-//        AlbumSummary album = conversionService.convert("ok", AlbumSummary.class);
-//    }
-//
-//    public ByteArrayOutputStream saveContentToWord(List<AlbumSummary> albumList) {
-//        return null;
-//    }
-//
-//    public ByteArrayInputStream saveAlbum(String urlLink) {
-//        return null;
-//    }
-
-
-
-
-
 }
