ec319eb8a91cd8823024d13814c5935e9c2d07d2
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-04-10 19:12:04 +0300

    Deleted comments

--- src/main/java/com/spring/api/music_project/controller/ApiController.java
+++ src/main/java/com/spring/api/music_project/controller/ApiController.java
@@ -1,7 +1,6 @@
 package com.spring.api.music_project.controller;
 
 import com.spring.api.music_project.model.AlbumSummary;
-import com.spring.api.music_project.model.service.ConverterService;
 import com.spring.api.music_project.model.service.IMusicService;
 import com.spring.api.music_project.model.service.Savable;
 import org.apache.log4j.Logger;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 @RestController
@@ -21,18 +19,12 @@ public class ApiController {
     private static final Logger LOGGER = Logger.getLogger(ApiController.class);
 
     protected IMusicService musicService;
-    protected ConverterService converterService;
     protected Savable albumStorage;
 
     public ApiController(IMusicService musicService, Savable albumStorage) {
         this.musicService = musicService;
         this.albumStorage = albumStorage;
     }
-
-    //    @GetMapping(value = "/album")
-//    public List<AlbumSummary> searchAll() {
-//        return musicService.printAllInfo();
-//    }
 
     @GetMapping(value = "/album/{signerName}/{albumTitle}")
     public ResponseEntity<List<AlbumSummary>> getAlbum(@PathVariable(name = "signerName") String signerName,
@@ -41,17 +33,9 @@ public class ApiController {
         return new ResponseEntity<>(summaryList, HttpStatus.OK);
     }
 
-//    @GetMapping("/download/{signerName}/{albumTitle}/word")
-//    public ResponseEntity<byte[]> downloadWordDocument(@PathVariable(name = "signerName") String signerName,
-//                                               @PathVariable(name = "albumTitle") String albumTitle) {
-//        byte[] document = albumStorage.generateDoc();
-//        return ResponseEntity.ok().body(document);
-//    }
-
     @GetMapping("/album/{signerName}/{albumTitle}/word")
     public ResponseEntity<byte[]> downloadWordDocument(@PathVariable(name = "signerName") String signerName,
                                                        @PathVariable(name = "albumTitle") String albumTitle) {
-        ByteArrayOutputStream baos;
         byte[] document = albumStorage.saveAlbum(signerName, albumTitle).toByteArray();;
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION,
