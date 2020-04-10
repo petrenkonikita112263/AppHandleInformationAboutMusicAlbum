8bc7e70ba0cba41b5f51a4c798978ffed41d5a70
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-04-10 19:10:23 +0300

    New attempt to make response url - album/{name}/{name}. But the issue that user should type =json in url album=json/{name}/{name}

--- src/main/java/com/spring/api/music_project/model/service/MusicService.java
+++ src/main/java/com/spring/api/music_project/model/service/MusicService.java
@@ -1,7 +1,6 @@
 package com.spring.api.music_project.model.service;
 
 import com.spring.api.music_project.model.AlbumSummary;
-import com.spring.api.music_project.model.convert.ContentConverter;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheConfig;
@@ -20,12 +19,10 @@ public class MusicService implements IMusicService {
 
     private final UriComponentsBuilder uriComponentsBuilder;
 
-    private ContentConverter converter;
-
     private ConversionService conversionService;
 
-        public MusicService(@Value("${api_key}") String key,
-                            ConversionService conversionService) {
+    public MusicService(@Value("${api_key}") String key,
+                        ConversionService conversionService) {
         this.uriComponentsBuilder = UriComponentsBuilder
                 .fromHttpUrl("http://ws.audioscrobbler.com/2.0/?method=album.getinfo")
                 .queryParam("api_key", key);
@@ -40,9 +37,12 @@ public class MusicService implements IMusicService {
     @Override
     @Cacheable
     public List<AlbumSummary> obtaineAlbumThroughName(String nameOfArtist, String titleOfAlbum) {
-       String urlLink = uriComponentsBuilder.cloneBuilder().queryParam("artist",
-               nameOfArtist).queryParam("album", titleOfAlbum).build().toString();
-       return conversionService.convert(urlLink, List.class);
+        String urlLink = uriComponentsBuilder.cloneBuilder()
+                .queryParam("format", "json")
+                .queryParam("artist", nameOfArtist)
+                .queryParam("album", titleOfAlbum)
+                .build().toString();
+        return conversionService.convert(urlLink, List.class);
     }
 
 }
