8dc08a8e62fc058469ca9f3c8b0eef32af18c323
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-04-11 20:04:57 +0300

    Now application save information about album in template docx file, that has header with time and date.

--- src/main/java/com/spring/api/music_project/model/service/AlbumStorage.java
+++ src/main/java/com/spring/api/music_project/model/service/AlbumStorage.java
@@ -42,117 +42,127 @@ public class AlbumStorage implements Savable {
         if (!file.exists()) {
             createFileWithFolder();
         }
-        XWPFDocument document = new XWPFDocument();
+        FileInputStream fis = null;
         try {
-            XWPFParagraph h1 = document.createParagraph();
-            h1.setAlignment(ParagraphAlignment.CENTER);
-            XWPFParagraph image = document.createParagraph();
-            image.setAlignment(ParagraphAlignment.CENTER);
-            XWPFParagraph title = document.createParagraph();
-            title.setAlignment(ParagraphAlignment.DISTRIBUTE);
-            XWPFParagraph name = document.createParagraph();
-            name.setAlignment(ParagraphAlignment.DISTRIBUTE);
-            XWPFParagraph genre = document.createParagraph();
-            genre.setAlignment(ParagraphAlignment.DISTRIBUTE);
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-            XWPFRun h1Run = h1.createRun();
-            h1Run.setText("The Response Generated WORD Document");
-            h1Run.setBold(true);
-            h1Run.setFontFamily("Times New Roman");
-            h1Run.setFontSize(18);
-            h1Run.addBreak();
-            h1Run.setText("Made in Java Spring Boot using Last.fm API");
-            h1Run.setBold(true);
-            h1Run.setFontFamily("Times New Roman");
-            h1Run.setFontSize(15);
-            h1Run.addBreak();
-
-            for (AlbumSummary content : summaryList) {
-                XWPFRun imageRun = image.createRun();
-                if (content.getListOfPosters().isEmpty()) {
-                    imageRun.setText("The found album doesn't have any posters");
-                } else {
-                    for (int i = 2; i < content.getListOfPosters().size(); i += 10) {
-                        try {
-                            imageRun.addPicture(getImage(content.getListOfPosters().get(i).getStorageUrl()),
-                                    XWPFDocument.PICTURE_TYPE_PNG, "",
-                                    Units.toEMU(200), Units.toEMU(200));
-                        } catch (IOException e) {
-                            LOGGER.error("Can't input the image ", e);
-                        } catch (InvalidFormatException e) {
-                            LOGGER.error("Invalid format ", e);
-                        }
-                    }
-                }
-                imageRun.addBreak();
-                imageRun.setTextPosition(20);
-                imageRun.setText("Fig. Main Poster Of Album");
-                imageRun.addBreak();
-                imageRun.addBreak();
-                XWPFRun titleRun = title.createRun();
-                titleRun.setText("The Album Name is: " + content.getAlbumTitle());
-                titleRun.setFontFamily("Times New Roman");
-                titleRun.setFontSize(14);
-                XWPFRun nameRun = name.createRun();
-                nameRun.setText("The Band|Artist|Signer name is: " + content.getArtistName());
-                nameRun.setFontFamily("Times New Roman");
-                nameRun.setFontSize(14);
-                nameRun.addBreak();
-                nameRun.addBreak();
-                XWPFRun genreRun = genre.createRun();
-                genreRun.setFontFamily("Times New Roman");
-                genreRun.setFontSize(14);
-                if (content.getTags().isEmpty()) {
-                    genreRun.setText("Album don't have information about genre of music");
-                } else {
-                    String text = null;
-                    genreRun.setText("The Genre Of Music:");
-                    for (int i = 0; i < content.getTags().size(); i++) {
-                        text = " " + content.getTags().get(i).getName() + " ";
-                        genreRun.setText(text);
-                    }
-                }
-                genreRun.addBreak();
-                genreRun.addBreak();
-                XWPFRun trackRun = title.createRun();
-                if (content.getCollectionOfTracks().isEmpty()) {
-                    trackRun.setText("There're no tracks in album");
-                } else {
-                    XWPFTable table = document.createTable();
-                    XWPFTableRow mainRow = table.getRow(0);
-                    mainRow.getCell(0).setText("Name Of Track");
-                    mainRow.addNewTableCell().setText("Track Duration (X:XX, MI:SS)");
-                    for (int i = 0; i < content.getCollectionOfTracks().size(); i++) {
-                        XWPFTableRow contentRow = table.createRow();
-                        XWPFTableCell column_1 = contentRow.getCell(0);
-                        if (column_1 != null) {
-                            column_1.setText(content.getCollectionOfTracks().get(i).getTrackName());
-                        }
-                        XWPFTableCell column_2 = contentRow.getCell(1);
-                        if (column_2 != null) {
-                            column_2.setText(content.getCollectionOfTracks().get(i).getDuration());
-                        }
-                    }
-                }
-            }
+        try (XWPFDocument document = new XWPFDocument(fis)) {
             try {
-                document.write(baos);
-            } catch (IOException e) {
-                LOGGER.error("Some went wrong in process writing into file ", e);
+                XWPFParagraph h1 = document.createParagraph();
+                h1.setAlignment(ParagraphAlignment.CENTER);
+                XWPFParagraph image = document.createParagraph();
+                image.setAlignment(ParagraphAlignment.CENTER);
+                XWPFParagraph title = document.createParagraph();
+                title.setAlignment(ParagraphAlignment.DISTRIBUTE);
+                XWPFParagraph name = document.createParagraph();
+                name.setAlignment(ParagraphAlignment.DISTRIBUTE);
+                XWPFParagraph genre = document.createParagraph();
+                genre.setAlignment(ParagraphAlignment.DISTRIBUTE);
+
+                XWPFRun h1Run = h1.createRun();
+                h1Run.setText("The Response Generated WORD Document");
+                h1Run.setBold(true);
+                h1Run.setFontFamily("Times New Roman");
+                h1Run.setFontSize(18);
+                h1Run.addBreak();
+                h1Run.setText("Made in Java Spring Boot using Last.fm API");
+                h1Run.setBold(true);
+                h1Run.setFontFamily("Times New Roman");
+                h1Run.setFontSize(15);
+                h1Run.addBreak();
+
+                for (AlbumSummary content : summaryList) {
+                    XWPFRun imageRun = image.createRun();
+                    if (content.getListOfPosters().isEmpty()) {
+                        imageRun.setText("The found album doesn't have any posters");
+                    } else {
+                        for (int i = 2; i < content.getListOfPosters().size(); i += 10) {
+                            try {
+                                imageRun.addPicture(getImage(content.getListOfPosters().get(i).getStorageUrl()),
+                                        XWPFDocument.PICTURE_TYPE_PNG, "",
+                                        Units.toEMU(200), Units.toEMU(200));
+                            } catch (IOException e) {
+                                LOGGER.error("Can't input the image ", e);
+                            } catch (InvalidFormatException e) {
+                                LOGGER.error("Invalid format ", e);
+                            }
+                        }
+                    }
+                    imageRun.addBreak();
+                    imageRun.setTextPosition(20);
+                    imageRun.setText("Fig. Main Poster Of Album");
+                    imageRun.addBreak();
+                    imageRun.addBreak();
+                    XWPFRun titleRun = title.createRun();
+                    titleRun.setText("The Album Name is: " + content.getAlbumTitle());
+                    titleRun.setFontFamily("Times New Roman");
+                    titleRun.setFontSize(14);
+                    XWPFRun nameRun = name.createRun();
+                    nameRun.setText("The Band|Artist|Signer name is: " + content.getArtistName());
+                    nameRun.setFontFamily("Times New Roman");
+                    nameRun.setFontSize(14);
+                    nameRun.addBreak();
+                    nameRun.addBreak();
+                    XWPFRun genreRun = genre.createRun();
+                    genreRun.setFontFamily("Times New Roman");
+                    genreRun.setFontSize(14);
+                    if (content.getTags().isEmpty()) {
+                        genreRun.setText("Album don't have information about genre of music");
+                    } else {
+                        String text = null;
+                        genreRun.setText("The Genre Of Music:");
+                        for (int i = 0; i < content.getTags().size(); i++) {
+                            text = " " + content.getTags().get(i).getName() + " ";
+                            genreRun.setText(text);
+                        }
+                    }
+                    genreRun.addBreak();
+                    genreRun.addBreak();
+                    XWPFRun trackRun = title.createRun();
+                    if (content.getCollectionOfTracks().isEmpty()) {
+                        trackRun.setText("There're no tracks in album");
+                    } else {
+                        XWPFTable table = document.createTable();
+                        XWPFTableRow mainRow = table.getRow(0);
+                        mainRow.getCell(0).setText("Name Of Track");
+                        mainRow.addNewTableCell().setText("Track Duration (X:XX, MI:SS)");
+                        for (int i = 0; i < content.getCollectionOfTracks().size(); i++) {
+                            XWPFTableRow contentRow = table.createRow();
+                            XWPFTableCell column_1 = contentRow.getCell(0);
+                            if (column_1 != null) {
+                                column_1.setText(content.getCollectionOfTracks().get(i).getTrackName());
+                            }
+                            XWPFTableCell column_2 = contentRow.getCell(1);
+                            if (column_2 != null) {
+                                column_2.setText(content.getCollectionOfTracks().get(i).getDuration());
+                            }
+                        }
+                    }
+                }
+                try {
+                    document.write(baos);
+                } catch (IOException e) {
+                    LOGGER.error("Some went wrong in process writing into file ", e);
+                } finally {
+                    try {
+                        baos.flush();
+                        baos.close();
+                    } catch (IOException e) {
+                        LOGGER.error("Can't close output stream ", e);
+                    }
+                }
             } finally {
                 try {
-                    baos.flush();
-                    baos.close();
+                    document.close();
                 } catch (IOException e) {
-                    LOGGER.error("Can't close output stream ", e);
+                    LOGGER.error("Can't close document stream ", e);
                 }
             }
-        } finally {
-            try {
-                document.close();
-            } catch (IOException e) {
-                LOGGER.error("Can't close document stream ", e);
-            }
+        } catch (IOException e) {
+            LOGGER.error("Can't close document-reader stream ", e);
         }
         return baos;
     }
