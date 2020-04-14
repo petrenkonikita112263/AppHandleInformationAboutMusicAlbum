44dcae6e1614339a9583de9baba7a90519fb8f6f
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-04-14 12:14:38 +0300

    Added javadocs, fixed the generation of empty docx file if there's no template

--- src/main/java/com/spring/api/music_project/model/service/AlbumStorage.java
+++ src/main/java/com/spring/api/music_project/model/service/AlbumStorage.java
@@ -18,20 +18,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+/**
+ * Class marked as service, that gets the list of the albums, and writes
+ * the information into docx document using Apache POI.
+ * Caching the ByteArrayOutputStream.
+ */
 @Service
 @CacheConfig(cacheNames = {"baos"})
 public class AlbumStorage implements Savable {
 
+    /**
+     * Constant for this class that add logging functionality.
+     */
     private static final Logger LOGGER = Logger.getLogger(AlbumStorage.class);
 
     private IMusicService musicService;
     private String storagePoint;
 
+    /**
+     * Constructor with arguments.
+     *
+     * @param storagePoint - holds the path from properties file to the string
+     * @param musicService - object of interface class
+     */
     public AlbumStorage(@Value("${documentPath}") String storagePoint, IMusicService musicService) {
         this.storagePoint = storagePoint;
         this.musicService = musicService;
     }
 
+    /**
+     * Implementing method from interface class. Also this method marked as
+     * cacheable - the return value is now caching.
+     *
+     * @param summaryList - reads information about list of album and
+     *                    writes it to the docx document
+     * @return - stream of data that was sent to the buffer
+     */
     @Override
     @Cacheable
     public ByteArrayOutputStream createTemplateDocument(List<AlbumSummary> summaryList) {
@@ -43,115 +65,115 @@ public class AlbumStorage implements Savable {
         try (FileInputStream fis = new FileInputStream(file)) {
             try (XWPFDocument document = new XWPFDocument(fis)) {
 
-                    XWPFParagraph h1 = document.createParagraph();
-                    h1.setAlignment(ParagraphAlignment.CENTER);
-                    XWPFParagraph image = document.createParagraph();
-                    image.setAlignment(ParagraphAlignment.CENTER);
-                    XWPFParagraph title = document.createParagraph();
-                    title.setAlignment(ParagraphAlignment.DISTRIBUTE);
-                    XWPFParagraph name = document.createParagraph();
-                    name.setAlignment(ParagraphAlignment.DISTRIBUTE);
-                    XWPFParagraph genre = document.createParagraph();
-                    genre.setAlignment(ParagraphAlignment.DISTRIBUTE);
+                XWPFParagraph h1 = document.createParagraph();
+                h1.setAlignment(ParagraphAlignment.CENTER);
+                XWPFParagraph image = document.createParagraph();
+                image.setAlignment(ParagraphAlignment.CENTER);
+                XWPFParagraph title = document.createParagraph();
+                title.setAlignment(ParagraphAlignment.DISTRIBUTE);
+                XWPFParagraph name = document.createParagraph();
+                name.setAlignment(ParagraphAlignment.DISTRIBUTE);
+                XWPFParagraph genre = document.createParagraph();
+                genre.setAlignment(ParagraphAlignment.DISTRIBUTE);
 
-                    XWPFRun h1Run = h1.createRun();
-                    h1Run.setText("The Response Generated WORD Document");
-                    h1Run.setBold(true);
-                    h1Run.setFontFamily("Times New Roman");
-                    h1Run.setFontSize(18);
-                    h1Run.addBreak();
-                    h1Run.setText("Made in Java Spring Boot using Last.fm API");
-                    h1Run.setBold(true);
-                    h1Run.setFontFamily("Times New Roman");
-                    h1Run.setFontSize(15);
-                    h1Run.addBreak();
+                XWPFRun h1Run = h1.createRun();
+                h1Run.setText("The Response Generated WORD Document");
+                h1Run.setBold(true);
+                h1Run.setFontFamily("Times New Roman");
+                h1Run.setFontSize(18);
+                h1Run.addBreak();
+                h1Run.setText("Made in Java Spring Boot using Last.fm API");
+                h1Run.setBold(true);
+                h1Run.setFontFamily("Times New Roman");
+                h1Run.setFontSize(15);
+                h1Run.addBreak();
 
-                    for (AlbumSummary content : summaryList) {
-                        XWPFRun imageRun = image.createRun();
-                        if (content.getListOfPosters().isEmpty()) {
-                            imageRun.setText("The found album doesn't have any posters");
-                        } else {
-                            for (int i = 2; i < content.getListOfPosters().size(); i += 10) {
-                                String imgUrl = content.getListOfPosters().get(i).getStorageUrl();
-                                try(BufferedInputStream bis = new BufferedInputStream(new URL(imgUrl).openStream());) {
-                                    try {
-                                        imageRun.addPicture(bis, XWPFDocument.PICTURE_TYPE_PNG, "",
-                                                Units.toEMU(200), Units.toEMU(200));
-                                    } catch (InvalidFormatException e) {
-                                        LOGGER.error("Invalid type format", e);
-                                    }
-                                } catch (IOException e) {
-                                    LOGGER.error("Can't close BufferedInputStream with ");
+                for (AlbumSummary content : summaryList) {
+                    XWPFRun imageRun = image.createRun();
+                    if (content.getListOfPosters().isEmpty()) {
+                        imageRun.setText("The found album doesn't have any posters");
+                    } else {
+                        for (int i = 2; i < content.getListOfPosters().size(); i += 10) {
+                            String imgUrl = content.getListOfPosters().get(i).getStorageUrl();
+                            try (BufferedInputStream bis = new BufferedInputStream(new URL(imgUrl).openStream());) {
+                                try {
+                                    imageRun.addPicture(bis, XWPFDocument.PICTURE_TYPE_PNG, "",
+                                            Units.toEMU(200), Units.toEMU(200));
+                                } catch (InvalidFormatException e) {
+                                    LOGGER.error("Invalid type format", e);
                                 }
-                            }
-                        }
-
-                        imageRun.addBreak();
-                        imageRun.setTextPosition(20);
-                        imageRun.setText("Fig. Main Poster Of Album");
-                        imageRun.addBreak();
-                        imageRun.addBreak();
-                        XWPFRun titleRun = title.createRun();
-                        titleRun.setText("The Album Name is: " + content.getAlbumTitle());
-                        titleRun.setFontFamily("Times New Roman");
-                        titleRun.setFontSize(14);
-                        XWPFRun nameRun = name.createRun();
-                        nameRun.setText("The Band|Artist|Signer name is: " + content.getArtistName());
-                        nameRun.setFontFamily("Times New Roman");
-                        nameRun.setFontSize(14);
-                        nameRun.addBreak();
-                        nameRun.addBreak();
-                        XWPFRun genreRun = genre.createRun();
-                        genreRun.setFontFamily("Times New Roman");
-                        genreRun.setFontSize(14);
-                        if (content.getTags().isEmpty()) {
-                            genreRun.setText("Album don't have information about genre of music");
-                        } else {
-                            String text;
-                            genreRun.setText("The Genre Of Music:");
-                            for (int i = 0; i < content.getTags().size(); i++) {
-                                text = " " + content.getTags().get(i).getName() + " ";
-                                genreRun.setText(text);
-                            }
-                        }
-                        genreRun.addBreak();
-                        genreRun.addBreak();
-                        XWPFRun trackRun = title.createRun();
-                        if (content.getCollectionOfTracks().isEmpty()) {
-                            trackRun.setText("There're no tracks in album");
-                        } else {
-                            XWPFTable table = document.createTable();
-                            table.setTableAlignment(TableRowAlign.CENTER);
-                            XWPFTableRow mainRow = table.getRow(0);
-                            mainRow.getCell(0).setText("Name Of Track");
-                            mainRow.addNewTableCell().setText("Track Duration (X:XX, MI:SS)");
-                            for (int i = 0; i < content.getCollectionOfTracks().size(); i++) {
-                                XWPFTableRow contentRow = table.createRow();
-                                XWPFTableCell column_1 = contentRow.getCell(0);
-                                column_1.setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
-                                if (column_1 != null) {
-                                    column_1.setText(content.getCollectionOfTracks().get(i).getTrackName());
-                                }
-                                XWPFTableCell column_2 = contentRow.getCell(1);
-                                column_2.setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
-                                if (column_2 != null) {
-                                    column_2.setText(content.getCollectionOfTracks().get(i).getDuration());
-                                }
+                            } catch (IOException e) {
+                                LOGGER.error("Can't close BufferedInputStream with ");
                             }
                         }
                     }
+
+                    imageRun.addBreak();
+                    imageRun.setTextPosition(20);
+                    imageRun.setText("Fig. Main Poster Of Album");
+                    imageRun.addBreak();
+                    imageRun.addBreak();
+                    XWPFRun titleRun = title.createRun();
+                    titleRun.setText("The Album Name is: " + content.getAlbumTitle());
+                    titleRun.setFontFamily("Times New Roman");
+                    titleRun.setFontSize(14);
+                    XWPFRun nameRun = name.createRun();
+                    nameRun.setText("The Band|Artist|Signer name is: " + content.getArtistName());
+                    nameRun.setFontFamily("Times New Roman");
+                    nameRun.setFontSize(14);
+                    nameRun.addBreak();
+                    nameRun.addBreak();
+                    XWPFRun genreRun = genre.createRun();
+                    genreRun.setFontFamily("Times New Roman");
+                    genreRun.setFontSize(14);
+                    if (content.getTags().isEmpty()) {
+                        genreRun.setText("Album don't have information about genre of music");
+                    } else {
+                        String text;
+                        genreRun.setText("The Genre Of Music:");
+                        for (int i = 0; i < content.getTags().size(); i++) {
+                            text = " " + content.getTags().get(i).getName() + " ";
+                            genreRun.setText(text);
+                        }
+                    }
+                    genreRun.addBreak();
+                    genreRun.addBreak();
+                    XWPFRun trackRun = title.createRun();
+                    if (content.getCollectionOfTracks().isEmpty()) {
+                        trackRun.setText("There're no tracks in album");
+                    } else {
+                        XWPFTable table = document.createTable();
+                        table.setTableAlignment(TableRowAlign.CENTER);
+                        XWPFTableRow mainRow = table.getRow(0);
+                        mainRow.getCell(0).setText("Name Of Track");
+                        mainRow.addNewTableCell().setText("Track Duration (X:XX, MI:SS)");
+                        for (int i = 0; i < content.getCollectionOfTracks().size(); i++) {
+                            XWPFTableRow contentRow = table.createRow();
+                            XWPFTableCell column_1 = contentRow.getCell(0);
+                            column_1.setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
+                            if (column_1 != null) {
+                                column_1.setText(content.getCollectionOfTracks().get(i).getTrackName());
+                            }
+                            XWPFTableCell column_2 = contentRow.getCell(1);
+                            column_2.setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
+                            if (column_2 != null) {
+                                column_2.setText(content.getCollectionOfTracks().get(i).getDuration());
+                            }
+                        }
+                    }
+                }
+                try {
+                    document.write(baos);
+                } catch (IOException e) {
+                    LOGGER.error("Some went wrong in process writing into file ", e);
+                } finally {
                     try {
-                        document.write(baos);
+                        baos.flush();
+                        baos.close();
                     } catch (IOException e) {
-                        LOGGER.error("Some went wrong in process writing into file ", e);
-                    } finally {
-                        try {
-                            baos.flush();
-                            baos.close();
-                        } catch (IOException e) {
-                            LOGGER.error("Can't close output stream ", e);
-                        }
+                        LOGGER.error("Can't close output stream ", e);
                     }
+                }
             } catch (IOException e) {
                 LOGGER.error("Can't close XWPFDocument using try-with-resources", e);
             }
@@ -163,16 +185,23 @@ public class AlbumStorage implements Savable {
         return baos;
     }
 
+    /**
+     * Additional method that creates default empty docs document.
+     */
     private void createFileWithFolder() {
         Path path = Paths.get(storagePoint);
         try {
             Files.createDirectories(path.getParent());
         } catch (IOException e) {
             LOGGER.error("Can't create folder by path", e);
-            e.printStackTrace();
         }
         try {
-            Files.createFile(path);
+            XWPFDocument document = new XWPFDocument();
+            FileOutputStream out = new FileOutputStream(
+                    new File(storagePoint));
+            document.write(out);
+            out.close();
+            document.close();
         } catch (FileAlreadyExistsException e) {
             LOGGER.error("Can't create file in folder", e);
         } catch (IOException e) {
@@ -180,6 +209,13 @@ public class AlbumStorage implements Savable {
         }
     }
 
+    /**
+     * Implemeting another method from interface Savable.
+     *
+     * @param nameOfArtist - string value with the name of the band|signer
+     * @param titleOfAlbum - string value with the name of the album
+     * @return - sent buffer data to the stream
+     */
     @Override
     public ByteArrayOutputStream saveAlbum(String nameOfArtist, String titleOfAlbum) {
         return createTemplateDocument(musicService.obtaineAlbumThroughName(nameOfArtist, titleOfAlbum));
--- src/main/java/com/spring/api/music_project/model/service/Savable.java
+++ src/main/java/com/spring/api/music_project/model/service/Savable.java
@@ -5,10 +5,27 @@ import com.spring.api.music_project.model.AlbumSummary;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
+/**
+ * Interface class for saving content of the album into docx document.
+ */
 public interface Savable {
 
+    /**
+     * Method that created docx file with the content of information.
+     *
+     * @param summaryList - reads information about list of album and
+     *                    writes it to the docx document
+     * @return - stream of data that was sent to the buffer
+     */
     ByteArrayOutputStream createTemplateDocument(List<AlbumSummary> summaryList);
 
+    /**
+     * Method that save album into stream.
+     *
+     * @param nameOfArtist - string value with the name of the band|signer
+     * @param titleOfAlbum - string value with the name of the album
+     * @return - sent buffer data to the stream
+     */
     ByteArrayOutputStream saveAlbum(String nameOfArtist, String titleOfAlbum);
 
 }
